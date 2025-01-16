8f73c6774cab4c4db573ebc9924e51f0484b95a8
daniilsapunov <145489531+daniilsapunov@users.noreply.github.com>
2025-01-16 19:26:41 +0300

    Delete app directory

--- app/chat_user/orientdb.py
+++
@@ -1,8 +0,0 @@
-import pyorient
-
-
-def connect_to_orientdb():
-    client = pyorient.OrientDB("localhost", 2424)
-    session_id = client.connect("root", "gure")
-    client.db_open("chat-bot-db", "root", "gure")
-    return client
--- app/chat_user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'chat_user'
-
-urlpatterns = [
-    path('', views.user_chat, name='chat'),
-]
