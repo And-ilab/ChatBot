c9c64c073ce89a3ae49135abaa35eaeb11cadab3
sold1er1K <sold1erus@yandex.ru>
2025-03-18 12:38:40 +0300

    setting.py change

--- app/app/settings.py
+++ app/app/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5000",
     "https:/www.digitranslab.com"
