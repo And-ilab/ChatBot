3875f38e72a06079fdaf1091a540b63cfc93c790
alia <alexaserko@gmail.com>
2025-03-26 12:28:53 +0300

    Included partitioned in response

--- app/chat_user/static/chat_user/js/apiFetch.js
+++ app/chat_user/static/chat_user/js/apiFetch.js
@@ -1,8 +1,18 @@
 const BASE_API_URL = 'https://www.chatbot.digitranslab.com';
 
-function apiFetch(endpoint, options = {}) {
+async function apiFetch(endpoint, options = {}) {
   const url = `${BASE_API_URL}${endpoint}`;
-  return fetch(url, options);
+  const response = await fetch(url, options);
+  if (response.ok) {
+    const cookies = response.headers.get('Set-Cookie');
+    if (cookies && cookies.includes('csrftoken')) {
+      response.headers['Set-Cookie'] = cookies.replace(
+        'csrftoken',
+        'csrftoken; SameSite=None; Secure; Partitioned'
+      );
+    }
+  }
+  return response;
 }
 
 window.apiFetch = apiFetch;
