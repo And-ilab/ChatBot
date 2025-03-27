e277c3d815dbdf32f44f58c0e77543fae88a9998
alia <alexaserko@gmail.com>
2025-03-27 12:34:21 +0300

    Changed apiFetch

--- app/chat_user/static/chat_user/js/apiFetch.js
+++ app/chat_user/static/chat_user/js/apiFetch.js
@@ -4,15 +4,13 @@ async function apiFetch(endpoint, options = {}) {
   const url = `${BASE_API_URL}${endpoint}`;
   const response = await fetch(url, options);
 
-  if (response.ok) {
-    const csrfToken = response.headers.get('X_CSRFTOKEN');
-    if (csrfToken) {
-      // Set the token in the cookies header
-      document.cookie = `csrftoken=${csrfToken}; SameSite=None; Secure; Partitioned`;
-    }
+  // Extract and set the CSRF token cookie regardless of response status
+  const csrfToken = response.headers.get('X_CSRFTOKEN');
+  if (csrfToken) {
+    document.cookie = `csrftoken=${csrfToken}; SameSite=None; Secure; Partitioned`;
   }
 
-  return response;
+  return response; // Return the response object as is
 }
 
 window.apiFetch = apiFetch;
